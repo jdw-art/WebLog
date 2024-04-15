e79b40a37952eedbbebf57f4ebafc61eeb8b2a83
jdw-art <jdw17733356203@icloud.com>
2024-04-15 21:56:37 +0800

    project initalization

---
+++ weblog-admin/src/test/java/com/jacob/weblog/admin/WeblogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.weblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ weblog-common/src/test/java/com/jacob/weblog/common/WeblogCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.weblog.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

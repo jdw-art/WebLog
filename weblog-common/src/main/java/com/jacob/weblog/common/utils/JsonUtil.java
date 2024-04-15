d6d1a7a64c2b0f51d8ee0306fd205373618d2715
jdw-art <jdw17733356203@icloud.com>
2024-04-15 22:28:43 +0800

    config(weblog):JSON util class

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/utils/JsonUtil.java
@@ -0,0 +1,24 @@
+package com.jacob.weblog.common.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: Jacob
+ * @Description: JSON 工具类
+ * @Date: 2024/4/15 22:12
+ * @Version: 1.0
+ */
+@Slf4j
+public class JsonUtil {
+    private static final ObjectMapper INSTANCE = new ObjectMapper();
+
+    public static String toJsonString(Object obj) {
+        try {
+            return INSTANCE.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            return obj.toString();
+        }
+    }
+}

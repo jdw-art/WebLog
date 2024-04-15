011c3dad315e676085c3be6fdf7df817d5277a60
jdw-art <jdw17733356203@icloud.com>
2024-04-15 22:30:43 +0800

    test(weblog-web):Test User model

---
+++ weblog-web/src/test/java/com/jacob/weblog/web/model/User.java
@@ -0,0 +1,17 @@
+package com.jacob.weblog.web.model;
+
+import lombok.Data;
+
+/**
+ * @Author: Jacob
+ * @Description: User 对象类
+ * @Date: 2024/4/15 22:16
+ * @Version: 1.0
+ */
+@Data
+public class User {
+    // 用户名
+    private String username;
+    // 性别
+    private Integer sex;
+}

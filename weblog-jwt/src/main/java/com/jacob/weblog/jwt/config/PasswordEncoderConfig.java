4adaf67070c71ed65a03996c33ff7cfe1922ae00
jdw-art <jdw17733356203@icloud.com>
2024-04-16 20:17:50 +0800

    feat(weblog-jwt):Password encryption

---
+++ weblog-jwt/src/main/java/com/jacob/weblog/jwt/config/PasswordEncoderConfig.java
@@ -0,0 +1,27 @@
+package com.jacob.weblog.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: Jacob
+ * @Description: 密码加密
+ * @Date: 2024/4/16 19:49
+ * @Version: 1.0
+ */
+@Component
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt 是一种安全且适合密码存储的哈希算法，它在进行哈希时会自动加入“盐”，增加密码的安全性。
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("jacob"));
+    }
+}

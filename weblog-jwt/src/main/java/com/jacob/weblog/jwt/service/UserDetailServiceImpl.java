c34db23c7cec287ca915e9fa239150d4e26780b2
jdw-art <jdw17733356203@icloud.com>
2024-04-16 20:18:58 +0800

    feat(weblog-jwt):User information implementation class

---
+++ weblog-jwt/src/main/java/com/jacob/weblog/jwt/service/UserDetailServiceImpl.java
@@ -0,0 +1,44 @@
+package com.jacob.weblog.jwt.service;
+
+import com.jacob.weblog.common.domain.dos.UserDO;
+import com.jacob.weblog.common.domain.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+/**
+ * @Author: Jacob
+ * @Description: 用户信息实现类
+ * @Date: 2024/4/16 19:50
+ * @Version: 1.0
+ */
+@Service
+@Slf4j
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 从数据库中查询
+        UserDO userDO = userMapper.findByUsername(username);
+
+        // 判断用户是否存在
+        if (Objects.isNull(userDO)) {
+            throw new UsernameNotFoundException("该用户不存在");
+        }
+
+        // authorities 用于指定角色，这里写死为 ADMIN 管理员
+        return User.withUsername(userDO.getUsername())
+                .password(userDO.getPassword())
+                .authorities("ADMIN")
+                .build();
+    }
+}

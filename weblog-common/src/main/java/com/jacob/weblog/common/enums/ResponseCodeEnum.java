7f26abf8947a1c9b14ff8a77bf0eeba40cc8f350
jdw-art <jdw17733356203@icloud.com>
2024-04-16 12:46:40 +0800

    feat(weblog-common):Response exception code

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/enums/ResponseCodeEnum.java
@@ -0,0 +1,27 @@
+package com.jacob.weblog.common.enums;
+
+import com.jacob.weblog.common.exception.BaseExceptionInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @Author: Jacob
+ * @Description: 响应异常码
+ * @Date: 2024/4/16 12:36
+ * @Version: 1.0
+ */
+@Getter
+@AllArgsConstructor
+public enum ResponseCodeEnum implements BaseExceptionInterface {
+    // ----------- 通用异常状态码 -----------
+    SYSTEM_ERROR("10000", "出错啦，后台小哥正在努力修复中..."),
+
+    // ----------- 业务异常状态码 -----------
+    PRODUCT_NOT_FOUND("20000", "该产品不存在（测试使用）"),
+    ;
+
+    // 异常码
+    private String errorCode;
+    // 错误信息
+    private String errorMessage;
+}

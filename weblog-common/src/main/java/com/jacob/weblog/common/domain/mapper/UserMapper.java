b30591272dfb1093d821de6e27a6bdf936309854
jdw-art <jdw17733356203@icloud.com>
2024-04-16 19:25:41 +0800

    build(weblog-common):User information interface class

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.jacob.weblog.common.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jacob.weblog.common.domain.dos.UserDO;
+
+/**
+ * @Author: Jacob
+ * @Description: 用户信息接口
+ * @Date: 2024/4/16 19:12
+ * @Version: 1.0
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}

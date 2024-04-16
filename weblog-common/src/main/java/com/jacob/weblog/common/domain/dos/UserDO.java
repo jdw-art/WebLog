1f4a0e5a8fa176c43141745457272e0ddbd5499a
jdw-art <jdw17733356203@icloud.com>
2024-04-16 19:25:29 +0800

    build(weblog-common):User information encapsulation class

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/dos/UserDO.java
@@ -0,0 +1,37 @@
+package com.jacob.weblog.common.domain.dos;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author: Jacob
+ * @Description: 用户信息封装类
+ * @Date: 2024/4/16 19:11
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_user")
+public class UserDO {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Boolean isDeleted;
+}

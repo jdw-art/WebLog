7fddd975093c11720b2b3f2032b5fd59357cad97
jdw-art <jdw17733356203@icloud.com>
2024-04-16 19:31:26 +0800

    build(weblog-jwt):weblog-jwt module configuration

---
+++ weblog-jwt/src/test/java/com/jacob/weblog/jwt/WeblogJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.weblog.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

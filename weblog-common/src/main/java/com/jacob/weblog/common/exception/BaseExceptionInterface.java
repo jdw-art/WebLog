69d30b3794208ac3a3d03baf6b49260b3e912114
jdw-art <jdw17733356203@icloud.com>
2024-04-16 12:46:14 +0800

    feat(weblog-common):Common exception interface

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/exception/BaseExceptionInterface.java
@@ -0,0 +1,13 @@
+package com.jacob.weblog.common.exception;
+
+/**
+ * @Author: Jacob
+ * @Description: 通用异常接口
+ * @Date: 2024/4/16 12:36
+ * @Version: 1.0
+ */
+public interface BaseExceptionInterface {
+    String getErrorCode();
+
+    String getErrorMessage();
+}

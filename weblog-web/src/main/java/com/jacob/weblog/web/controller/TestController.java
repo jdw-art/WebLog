75f9033efd111bbe1270641e8357d96de658d1b8
jdw-art <jdw17733356203@icloud.com>
2024-04-17 21:27:26 +0800

    add(weblog-web):Add update test interface

--- weblog-web/src/main/java/com/jacob/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/jacob/weblog/web/controller/TestController.java
@@ -9,6 +9,7 @@ import com.jacob.weblog.web.model.User;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -43,5 +44,14 @@ public class TestController {
         user.setTime(LocalTime.now());
 
         return Response.success(user);
+    }
+
+    @PostMapping("/admin/update")
+    @ApiOperationLog(description = "测试更新接口")
+    @ApiOperation(value = "测试更新接口")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Response testUpdate() {
+        log.info("更新成功...");
+        return Response.success();
     }
 }

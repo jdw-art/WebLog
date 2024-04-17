4d700986aa45ee2258534b17e8fcd3b792ca92e9
jdw-art <jdw17733356203@icloud.com>
2024-04-17 14:45:31 +0800

    feat(weblog-admin):Spring Security configuration class

--- weblog-admin/src/main/java/com/jacob/weblog/admin/config/WebSecurityConfig.java
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/config/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.jacob.weblog.admin.config;
 
 import com.jacob.weblog.jwt.config.JwtAuthenticationSecurityConfig;
+import com.jacob.weblog.jwt.filter.TokenAuthenticationFilter;
+import com.jacob.weblog.jwt.handler.RestAccessDeniedHandler;
+import com.jacob.weblog.jwt.handler.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @Author: Jacob
@@ -20,6 +25,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationSecurityConfig jwtAuthenticationSecurityConfig;
+    @Autowired
+    private RestAuthenticationEntryPoint authEntryPoint;
+    @Autowired
+    private RestAccessDeniedHandler deniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,6 +40,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin/**").authenticated() // 认证所有以 /admin 为前缀的 URL 资源
                 .anyRequest().permitAll() // 其他都需要放行，无需认证
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 前后端分离，无需创建会话
+                .httpBasic().authenticationEntryPoint(authEntryPoint) // 处理用户未登录访问受保护的资源的情况
+                .and()
+                .exceptionHandling().accessDeniedHandler(deniedHandler) // 处理登录成功后访问受保护的资源，但是权限不够的情况
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 前后端分离，无需创建会话
+                .and()
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class) // 将 Token 校验过滤器添加到用户认证过滤器之前
+        ;
+    }
+
+    /**
+     * Token 校验过滤器
+     * @return
+     */
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter();
     }
 }

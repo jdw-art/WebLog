b9bb779ade6d24bdec3fca65ec86bdab76e8239d
jdw-art <jdw17733356203@icloud.com>
2024-04-17 21:57:30 +0800

    Add(weblog-admin):User information entity class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/UpdateAdminUserPasswordReqVO.java
@@ -0,0 +1,31 @@
+package com.jacob.weblog.admin.model.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @Author: Jacob
+ * @Description: 用户信息实体类
+ * @Date: 2024/4/17 21:50
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "修改用户密码 VO")
+public class UpdateAdminUserPasswordReqVO {
+    @NotBlank(message = "用户名不能为空")
+    @ApiModelProperty(value = "用户名")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    @ApiModelProperty(value = "密码")
+    private String password;
+}

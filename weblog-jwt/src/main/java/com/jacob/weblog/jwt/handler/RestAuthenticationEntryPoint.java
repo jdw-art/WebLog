29da92686b76499633e9ff8bd9f6dc380dc19809
jdw-art <jdw17733356203@icloud.com>
2024-04-17 14:44:47 +0800

    feat(weblog-jwt):Access protected resource classes

---
+++ weblog-jwt/src/main/java/com/jacob/weblog/jwt/handler/RestAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.jacob.weblog.jwt.handler;
+
+import com.jacob.weblog.common.enums.ResponseCodeEnum;
+import com.jacob.weblog.common.utils.Response;
+import com.jacob.weblog.jwt.utils.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: Jacob
+ * @Description: 访问受保护资源类
+ * @Date: 2024/4/17 11:10
+ * @Version: 1.0
+ */
+@Component
+@Slf4j
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.warn("用户未登录访问受保护的资源: ", authException);
+        if (authException instanceof InsufficientAuthenticationException) {
+            ResultUtil.fail(response, HttpStatus.UNAUTHORIZED.value(), Response.fail(ResponseCodeEnum.UNAUTHORIZED));
+        }
+
+        ResultUtil.fail(response, HttpStatus.UNAUTHORIZED.value(), Response.fail(authException.getMessage()));
+    }
+}

9640530b6ae5322681b3f5973af471239a0049e4
jdw-art <jdw17733356203@icloud.com>
2024-04-17 21:55:59 +0800

    feat(weblog-admin):User business interface

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/service/AdminUserService.java
@@ -0,0 +1,19 @@
+package com.jacob.weblog.admin.service;
+
+import com.jacob.weblog.admin.model.vo.UpdateAdminUserPasswordReqVO;
+import com.jacob.weblog.common.utils.Response;
+
+/**
+ * @Author: Jacob
+ * @Description: 用户业务接口
+ * @Date: 2024/4/17 21:51
+ * @Version: 1.0
+ */
+public interface AdminUserService {
+    /**
+     * 修改密码
+     * @param updateAdminUserPasswordReqVO
+     * @return
+     */
+    Response updatePassword(UpdateAdminUserPasswordReqVO updateAdminUserPasswordReqVO);
+}

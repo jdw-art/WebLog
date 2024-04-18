4154a0884030a67965a09bd3097da6b6096c7271
jdw-art <jdw17733356203@icloud.com>
2024-04-18 22:13:54 +0800

    feat(file):File management

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/controller/AdminFileController.java
@@ -0,0 +1,34 @@
+package com.jacob.weblog.admin.controller;
+
+import com.jacob.weblog.admin.service.AdminFileService;
+import com.jacob.weblog.common.aspect.ApiOperationLog;
+import com.jacob.weblog.common.utils.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Author: Jacob
+ * @Description: 文件管理
+ * @Date: 2024/4/18 20:55
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/admin")
+@Api(tags = "Admin 文件模块")
+public class AdminFileController {
+    @Autowired
+    private AdminFileService fileService;
+
+    @PostMapping("/file/upload")
+    @ApiOperation(value = "文件上传")
+    @ApiOperationLog(description = "文件上传")
+    public Response uploadFile(@RequestParam MultipartFile file) {
+        return fileService.uploadFile(file);
+    }
+}

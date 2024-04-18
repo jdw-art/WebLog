a0e9403a148f28d001b319c1e4e0f1b6b9e55e58
jdw-art <jdw17733356203@icloud.com>
2024-04-18 16:36:59 +0800

    feat(category):Query input parameter entity class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/FindCategoryPageListReqVO.java
@@ -0,0 +1,38 @@
+package com.jacob.weblog.admin.model.vo;
+
+import com.jacob.weblog.common.model.BasePageQuery;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * @Author: Jacob
+ * @Description: 查询入参实体类
+ * @Date: 2024/4/18 16:29
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "查询分类分页数据入参 VO")
+public class FindCategoryPageListReqVO extends BasePageQuery {
+    /**
+     * 分类名称
+     */
+    private String name;
+
+    /**
+     * 创建的起始日期
+     */
+    private LocalDate startDate;
+
+    /**
+     * 创建的结束日期
+     */
+    private LocalDate endDate;
+}

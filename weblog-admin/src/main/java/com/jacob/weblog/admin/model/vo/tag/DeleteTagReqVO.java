65f7f490ced0c39b5439ed051a732b8d73afe143
jdw-art <jdw17733356203@icloud.com>
2024-04-18 20:43:29 +0800

    feat(tag):Delete tag parameter class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/tag/DeleteTagReqVO.java
@@ -0,0 +1,25 @@
+package com.jacob.weblog.admin.model.vo.tag;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Jacob
+ * @Description: 删除标签参数类
+ * @Date: 2024/4/18 20:08
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "删除标签 VO")
+public class DeleteTagReqVO {
+    @NotNull(message = "标签 ID 不能为空")
+    private Long id;
+}

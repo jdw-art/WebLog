2095ca8bee198dd0124f7f78715d996dca956da5
jdw-art <jdw17733356203@icloud.com>
2024-04-18 16:24:43 +0800

    feat(weblog-admin):Article classification management interface class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/service/AdminCategoryService.java
@@ -0,0 +1,19 @@
+package com.jacob.weblog.admin.service;
+
+import com.jacob.weblog.admin.model.vo.category.AddCategoryReqVO;
+import com.jacob.weblog.common.utils.Response;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章分类管理接口类
+ * @Date: 2024/4/18 16:18
+ * @Version: 1.0
+ */
+public interface AdminCategoryService {
+    /**
+     * 添加分类
+     * @param addCategoryReqVO
+     * @return
+     */
+    Response addCategory(AddCategoryReqVO addCategoryReqVO);
+}

71ee48504464e016e7b19a6cb815b2e20261efae
jdw-art <jdw17733356203@icloud.com>
2024-04-18 16:37:17 +0800

    feat(category):Query returns parameter entity class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/FindCategoryPageListRspVO.java
@@ -0,0 +1,35 @@
+package com.jacob.weblog.admin.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: Jacob
+ * @Description: 查询出参实体类
+ * @Date: 2024/4/18 16:31
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindCategoryPageListRspVO {
+    /**
+     * 分类 ID
+     */
+    private Long id;
+
+    /**
+     * 分类名称
+     */
+    private String name;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+}

fc0a80646ec25561cd089687a0192bff6c46db6c
jdw-art <jdw17733356203@icloud.com>
2024-04-18 20:42:33 +0800

    feat(tag):Article tag management

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/controller/AdminTagController.java
@@ -0,0 +1,60 @@
+package com.jacob.weblog.admin.controller;
+
+import com.jacob.weblog.admin.model.vo.tag.AddTagReqVO;
+import com.jacob.weblog.admin.model.vo.tag.DeleteTagReqVO;
+import com.jacob.weblog.admin.model.vo.tag.FindTagPageListReqVO;
+import com.jacob.weblog.admin.model.vo.tag.SearchTagsReqVO;
+import com.jacob.weblog.admin.service.AdminTagService;
+import com.jacob.weblog.common.aspect.ApiOperationLog;
+import com.jacob.weblog.common.utils.PageResponse;
+import com.jacob.weblog.common.utils.Response;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章标签管理
+ * @Date: 2024/4/18 20:04
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/admin")
+@Api(tags = "Admin 标签模块")
+public class AdminTagController {
+    @Autowired
+    private AdminTagService tagService;
+
+    @PostMapping("/tag/add")
+    @ApiOperation(value = "添加标签")
+    @ApiOperationLog(description = "添加标签")
+    public Response addTags(@RequestBody @Validated AddTagReqVO addTagReqVO) {
+        return tagService.addTags(addTagReqVO);
+    }
+
+    @PostMapping("/tag/list")
+    @ApiOperation(value = "标签分页数据获取")
+    @ApiOperationLog(description = "标签分页数据获取")
+    public PageResponse findTagPageList(@RequestBody @Validated FindTagPageListReqVO findTagPageListReqVO) {
+        return tagService.findTagPageList(findTagPageListReqVO);
+    }
+
+    @PostMapping("/tag/delete")
+    @ApiOperation(value = "删除标签")
+    @ApiOperationLog(description = "删除标签")
+    public Response deleteTag(@RequestBody @Validated DeleteTagReqVO deleteTagReqVO) {
+        return tagService.deleteTag(deleteTagReqVO);
+    }
+
+    @PostMapping("/tag/search")
+    @ApiOperation(value = "标签模糊查询")
+    @ApiOperationLog(description = "标签模糊查询")
+    public Response searchTags(@RequestBody @Validated SearchTagsReqVO searchTagsReqVO) {
+        return tagService.searchTags(searchTagsReqVO);
+    }
+}

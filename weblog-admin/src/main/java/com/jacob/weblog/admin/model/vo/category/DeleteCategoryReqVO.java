4fe42988f1a4b8a7f8e8fa4bfb3c93b014b3839f
jdw-art <jdw17733356203@icloud.com>
2024-04-18 18:16:29 +0800

    feat(category):Delete classification request wrapper class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/category/DeleteCategoryReqVO.java
@@ -0,0 +1,25 @@
+package com.jacob.weblog.admin.model.vo.category;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Jacob
+ * @Description: 删除分类请求包装类
+ * @Date: 2024/4/18 18:12
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "删除分类 VO")
+public class DeleteCategoryReqVO {
+    @NotNull(message = "分类 ID 不能为空")
+    private Long id;
+}

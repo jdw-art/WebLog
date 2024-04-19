125f160d4bfeb262a2b40f7df9b3e721e26274c0
jdw-art <jdw17733356203@icloud.com>
2024-04-19 12:48:25 +0800

    feat(setting):Add get blog setup details interface

--- weblog-admin/src/main/java/com/jacob/weblog/admin/service/impl/AdminBlogSettingsServiceImpl.java
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/service/impl/AdminBlogSettingsServiceImpl.java
@@ -1,12 +1,15 @@
 package com.jacob.weblog.admin.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jacob.weblog.admin.convert.BlogSettingsConvert;
+import com.jacob.weblog.admin.model.vo.blogsettings.FindBlogSettingsRspVO;
 import com.jacob.weblog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
 import com.jacob.weblog.admin.service.AdminBlogSettingsService;
 import com.jacob.weblog.common.domain.dos.BlogSettingsDO;
 import com.jacob.weblog.common.domain.mapper.BlogSettingsMapper;
 import com.jacob.weblog.common.utils.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,24 +21,34 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class AdminBlogSettingsServiceImpl extends ServiceImpl<BlogSettingsMapper, BlogSettingsDO> implements AdminBlogSettingsService {
+
+    @Autowired
+    private BlogSettingsMapper blogSettingsMapper;
+
     @Override
     public Response updateBlogSettings(UpdateBlogSettingsReqVO updateBlogSettingsReqVO) {
         // VO 转 DO
-        BlogSettingsDO blogSettingsDO = BlogSettingsDO.builder()
-                .id(1L)
-                .logo(updateBlogSettingsReqVO.getLogo())
-                .name(updateBlogSettingsReqVO.getName())
-                .author(updateBlogSettingsReqVO.getAuthor())
-                .introduction(updateBlogSettingsReqVO.getIntroduction())
-                .avatar(updateBlogSettingsReqVO.getAvatar())
-                .githubHomepage(updateBlogSettingsReqVO.getGithubHomepage())
-                .giteeHomepage(updateBlogSettingsReqVO.getGiteeHomepage())
-                .csdnHomepage(updateBlogSettingsReqVO.getCsdnHomepage())
-                .zhihuHomepage(updateBlogSettingsReqVO.getZhihuHomepage())
-                .build();
+        BlogSettingsDO blogSettingsDO = BlogSettingsConvert.INSTANCE.convertVO2DO(updateBlogSettingsReqVO);
+        blogSettingsDO.setId(1L);
 
         // 保存或更新（当数据库中存在 ID 为 1 的记录时，则执行更新操作，否则执行插入操作）
         saveOrUpdate(blogSettingsDO);
         return Response.success();
+    }
+
+    /**
+     * 获取博客设置详情
+     *
+     * @return
+     */
+    @Override
+    public Response findDetail() {
+        // 查询 ID 为 1 的记录
+        BlogSettingsDO blogSettingsDO = blogSettingsMapper.selectById(1L);
+
+        // DO 转 VO
+        FindBlogSettingsRspVO vo = BlogSettingsConvert.INSTANCE.convertDO2VO(blogSettingsDO);
+
+        return Response.success(vo);
     }
 }

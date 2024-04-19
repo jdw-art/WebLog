a06e242b35dce814694bf7b37784218fe1060628
jdw-art <jdw17733356203@icloud.com>
2024-04-19 12:13:21 +0800

    feat(setting):Blog settings information mapper

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/BlogSettingsMapper.java
@@ -0,0 +1,13 @@
+package com.jacob.weblog.common.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jacob.weblog.common.domain.dos.BlogSettingsDO;
+
+/**
+ * @Author: Jacob
+ * @Description: 博客设置信息mapper
+ * @Date: 2024/4/18 22:18
+ * @Version: 1.0
+ */
+public interface BlogSettingsMapper extends BaseMapper<BlogSettingsDO> {
+}

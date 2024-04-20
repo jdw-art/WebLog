0e3df2e52e363865c35740b46dda6423d345b849
jdw-art <jdw17733356203@icloud.com>
2024-04-20 22:03:38 +0800

    feat(article):Article content interface Mapper

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/ArticleContentMapper.java
@@ -0,0 +1,33 @@
+package com.jacob.weblog.common.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.jacob.weblog.common.domain.dos.ArticleContentDO;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章内容接口Mapper
+ * @Date: 2024/4/20 20:52
+ * @Version: 1.0
+ */
+public interface ArticleContentMapper extends BaseMapper<ArticleContentDO> {
+    /**
+     * 根据文章 ID 删除记录
+     * @param articleId
+     * @return
+     */
+    default int deleteByArticleId(Long articleId) {
+        return delete(Wrappers.<ArticleContentDO>lambdaQuery()
+                .eq(ArticleContentDO::getArticleId, articleId));
+    }
+
+    /**
+     * 根据文章 ID 查询
+     * @param articleId
+     * @return
+     */
+    default ArticleContentDO selectByArticleId(Long articleId) {
+        return selectOne(Wrappers.<ArticleContentDO>lambdaQuery()
+                .eq(ArticleContentDO::getArticleId, articleId));
+    }
+}

83c596fcb7f282653cae1a20e6e9b79349503e06
jdw-art <jdw17733356203@icloud.com>
2024-04-20 22:04:01 +0800

    feat(article):Article entity class

---
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/dos/ArticleDO.java
@@ -0,0 +1,42 @@
+package com.jacob.weblog.common.domain.dos;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章实体类
+ * @Date: 2024/4/20 20:49
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@TableName("t_article")
+public class ArticleDO {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String cover;
+
+    private String summary;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    private Boolean isDeleted;
+
+    private Long readNum;
+}

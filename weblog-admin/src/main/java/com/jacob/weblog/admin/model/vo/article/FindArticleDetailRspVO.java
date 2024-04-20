d02334f1978fd1eddb9d5bf1efd3f0a956b05e03
jdw-art <jdw17733356203@icloud.com>
2024-04-20 22:05:44 +0800

    feat(article):Get article details and enter parameter encapsulation class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/article/FindArticleDetailRspVO.java
@@ -0,0 +1,55 @@
+package com.jacob.weblog.admin.model.vo.article;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @Author: Jacob
+ * @Description: 获取文章详情入参封装类
+ * @Date: 2024/4/20 21:23
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindArticleDetailRspVO {
+    /**
+     * 文章 ID
+     */
+    private Long id;
+
+    /**
+     * 文章标题
+     */
+    private String title;
+
+    /**
+     * 文章封面
+     */
+    private String cover;
+
+    /**
+     * 文章内容
+     */
+    private String content;
+
+    /**
+     * 分类 ID
+     */
+    private Long categoryId;
+
+    /**
+     * 标签 ID 集合
+     */
+    private List<Long> tagIds;
+
+    /**
+     * 摘要
+     */
+    private String summary;
+}

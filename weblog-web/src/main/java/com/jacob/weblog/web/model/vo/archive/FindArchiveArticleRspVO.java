1210735b4c1710c625f59eec264d6e2609bc23e9
jdw-art <jdw17733356203@icloud.com>
2024-04-21 18:47:22 +0800

    feat(frontend-archive):Article archive parameter encapsulation class

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/archive/FindArchiveArticleRspVO.java
@@ -0,0 +1,34 @@
+package com.jacob.weblog.web.model.vo.archive;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章归档出参封装类
+ * @Date: 2024/4/21 16:44
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindArchiveArticleRspVO {
+    private Long id;
+    private String cover;
+    private String title;
+    /**
+     * 发布日期
+     */
+    private LocalDate createDate;
+
+    /**
+     * 发布的月份（此字段不需要展示在前端，主要用于按月份分组使用）
+     */
+    private YearMonth createMonth;
+}

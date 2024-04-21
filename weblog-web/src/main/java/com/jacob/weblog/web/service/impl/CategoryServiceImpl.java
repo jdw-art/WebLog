a1cbe2f2805b4064e2c2bc1c2402f3e5a4104a8e
jdw-art <jdw17733356203@icloud.com>
2024-04-21 20:34:36 +0800

    update(frontend-category):Add get pagination data of articles under category

--- weblog-web/src/main/java/com/jacob/weblog/web/service/impl/CategoryServiceImpl.java
+++ weblog-web/src/main/java/com/jacob/weblog/web/service/impl/CategoryServiceImpl.java
@@ -1,9 +1,20 @@
 package com.jacob.weblog.web.service.impl;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jacob.weblog.common.domain.dos.ArticleCategoryRelDO;
+import com.jacob.weblog.common.domain.dos.ArticleDO;
 import com.jacob.weblog.common.domain.dos.CategoryDO;
+import com.jacob.weblog.common.domain.mapper.ArticleCategoryRelMapper;
+import com.jacob.weblog.common.domain.mapper.ArticleMapper;
 import com.jacob.weblog.common.domain.mapper.CategoryMapper;
+import com.jacob.weblog.common.enums.ResponseCodeEnum;
+import com.jacob.weblog.common.exception.BizException;
+import com.jacob.weblog.common.utils.PageResponse;
 import com.jacob.weblog.common.utils.Response;
+import com.jacob.weblog.web.convert.ArticleConvert;
+import com.jacob.weblog.web.model.vo.category.FindCategoryArticlePageListReqVO;
+import com.jacob.weblog.web.model.vo.category.FindCategoryArticlePageListRspVO;
 import com.jacob.weblog.web.model.vo.category.FindCategoryListRspVO;
 import com.jacob.weblog.web.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -25,6 +37,10 @@ import java.util.stream.Collectors;
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
+    @Autowired
+    private ArticleCategoryRelMapper articleCategoryRelMapper;
+    @Autowired
+    private ArticleMapper articleMapper;
 
     /**
      * 获取分类列表
@@ -48,5 +64,51 @@ public class CategoryServiceImpl implements CategoryService {
         }
 
         return Response.success(vos);
+    }
+
+    /**
+     * 获取分类下文章分页数据
+     *
+     * @param findCategoryArticlePageListReqVO
+     * @return
+     */
+    @Override
+    public Response findCategoryArticlePageList(FindCategoryArticlePageListReqVO findCategoryArticlePageListReqVO) {
+        Long current = findCategoryArticlePageListReqVO.getCurrent();
+        Long size = findCategoryArticlePageListReqVO.getSize();
+        Long categoryId = findCategoryArticlePageListReqVO.getId();
+
+        CategoryDO categoryDO = categoryMapper.selectById(categoryId);
+
+        // 判断该分类是否存在
+        if (Objects.isNull(categoryDO)) {
+            log.warn("==> 该分类不存在, categoryId: {}", categoryId);
+            throw new BizException(ResponseCodeEnum.CATEGORY_NOT_EXISTED);
+        }
+
+        // 先查询该分类下所有关联的文章 ID
+        List<ArticleCategoryRelDO> articleCategoryRelDOS = articleCategoryRelMapper.selectListByCategoryId(categoryId);
+
+        // 若该分类下未发布任何文章
+        if (CollectionUtils.isEmpty(articleCategoryRelDOS)) {
+            log.info("==> 该分类下还未发布任何文章, categoryId: {}", categoryId);
+            return PageResponse.success(null, null);
+        }
+
+        List<Long> articleIds = articleCategoryRelDOS.stream().map(ArticleCategoryRelDO::getArticleId).collect(Collectors.toList());
+
+        // 根据文章 ID 集合查询文章分页数据
+        Page<ArticleDO> page = articleMapper.selectPageListByArticleIds(current, size, articleIds);
+        List<ArticleDO> articleDOS = page.getRecords();
+
+        // DO 转 VO
+        List<FindCategoryArticlePageListRspVO> vos = null;
+        if (!CollectionUtils.isEmpty(articleDOS)) {
+            vos = articleDOS.stream()
+                    .map(articleDO -> ArticleConvert.INSTANCE.convertDO2CategoryArticleVO(articleDO))
+                    .collect(Collectors.toList());
+        }
+
+        return PageResponse.success(page, vos);
     }
 }

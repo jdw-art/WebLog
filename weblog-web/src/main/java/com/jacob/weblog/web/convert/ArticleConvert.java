7c3d6abbc7a3a19b0b29d6c4ff22cc6498c37530
jdw-art <jdw17733356203@icloud.com>
2024-04-21 18:46:37 +0800

    update(frontend-archive):Convert DO to archived article VO

--- weblog-web/src/main/java/com/jacob/weblog/web/convert/ArticleConvert.java
+++ weblog-web/src/main/java/com/jacob/weblog/web/convert/ArticleConvert.java
@@ -1,6 +1,7 @@
 package com.jacob.weblog.web.convert;
 
 import com.jacob.weblog.common.domain.dos.ArticleDO;
+import com.jacob.weblog.web.model.vo.archive.FindArchiveArticleRspVO;
 import com.jacob.weblog.web.model.vo.article.FindIndexArticlePageListRspVO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -26,4 +27,13 @@ public interface ArticleConvert {
      */
     @Mapping(target = "createDate", expression = "java(java.time.LocalDate.from(bean.getCreateTime()))")
     FindIndexArticlePageListRspVO convertDO2VO(ArticleDO bean);
+
+    /**
+     * 将 DO 转化为归档文章 VO
+     * @param bean
+     * @return
+     */
+    @Mapping(target = "createDate", expression = "java(java.time.LocalDate.from(bean.getCreateTime()))")
+    @Mapping(target = "createMonth", expression = "java(java.time.YearMonth.from(bean.getCreateTime()))")
+    FindArchiveArticleRspVO convertDO2ArchiveArticleVO(ArticleDO bean);
 }

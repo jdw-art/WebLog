4579521f3962731ea0ba433dc2cec1dcd998832f
jdw-art <jdw17733356203@icloud.com>
2024-04-21 16:40:09 +0800

    feat(frontend-article):Article pagination response parameter class

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/article/FindIndexArticlePageListRspVO.java
@@ -0,0 +1,39 @@
+package com.jacob.weblog.web.model.vo.article;
+
+import com.jacob.weblog.web.model.vo.category.FindCategoryListRspVO;
+import com.jacob.weblog.web.model.vo.tag.FindTagListRspVO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章分页响应参数类
+ * @Date: 2024/4/21 14:08
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindIndexArticlePageListRspVO {
+    private Long id;
+    private String cover;
+    private String title;
+    private LocalDate createDate;
+    private String summary;
+    /**
+     * 文章分类
+     */
+    private FindCategoryListRspVO category;
+
+    /**
+     * 文章标签
+     */
+    private List<FindTagListRspVO> tags;
+}

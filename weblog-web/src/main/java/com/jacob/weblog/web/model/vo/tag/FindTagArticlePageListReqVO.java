355302cd73c9105b814f9036a4e9a2126f6a8dbb
jdw-art <jdw17733356203@icloud.com>
2024-04-21 21:26:09 +0800

    feat(frontend-tag):Article paging request class under tag

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/tag/FindTagArticlePageListReqVO.java
@@ -0,0 +1,27 @@
+package com.jacob.weblog.web.model.vo.tag;
+
+import com.jacob.weblog.common.model.BasePageQuery;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Jacob
+ * @Description: 标签下文章分页请求类
+ * @Date: 2024/4/21 20:35
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindTagArticlePageListReqVO extends BasePageQuery {
+    /**
+     * 标签 ID
+     */
+    @NotNull(message = "标签 ID 不能为空")
+    private Long id;
+}

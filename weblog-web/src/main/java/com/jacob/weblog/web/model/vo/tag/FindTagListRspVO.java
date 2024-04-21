1a7a4957d2a1c9d43be6666f64148156c6345f9b
jdw-art <jdw17733356203@icloud.com>
2024-04-21 16:40:20 +0800

    feat(frontend-tag):Query article tag encapsulation class

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/tag/FindTagListRspVO.java
@@ -0,0 +1,21 @@
+package com.jacob.weblog.web.model.vo.tag;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: Jacob
+ * @Description: 查询文章标签封装类
+ * @Date: 2024/4/21 14:07
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindTagListRspVO {
+    private Long id;
+    private String name;
+}

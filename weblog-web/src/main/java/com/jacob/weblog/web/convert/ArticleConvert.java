3d51e9e2385b3a43b72a4c8540e0f749f2e43f40
jdw-art <jdw17733356203@icloud.com>
2024-04-21 16:35:41 +0800

    feat(frontend-article):ArticleConvert conversion interface

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/convert/ArticleConvert.java
@@ -0,0 +1,29 @@
+package com.jacob.weblog.web.convert;
+
+import com.jacob.weblog.common.domain.dos.ArticleDO;
+import com.jacob.weblog.web.model.vo.article.FindIndexArticlePageListRspVO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * @Author: Jacob
+ * @Description: ArticleConvert 转换接口
+ * @Date: 2024/4/21 14:12
+ * @Version: 1.0
+ */
+@Mapper
+public interface ArticleConvert {
+    /**
+     * 初始化 convert 实例
+     */
+    ArticleConvert INSTANCE = Mappers.getMapper(ArticleConvert.class);
+
+    /**
+     * 将 DO 转化为 VO
+     * @param bean
+     * @return
+     */
+    @Mapping(target = "createDate", expression = "java(java.time.LocalDate.from(bean.getCreateTime()))")
+    FindIndexArticlePageListRspVO convertDO2VO(ArticleDO bean);
+}

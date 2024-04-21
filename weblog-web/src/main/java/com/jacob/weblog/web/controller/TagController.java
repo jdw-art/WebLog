e59e357089915acd425068520c67681d8edc27a5
jdw-art <jdw17733356203@icloud.com>
2024-04-21 16:40:33 +0800

    feat(frontend-tag):Article Tag Data Controller

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/controller/TagController.java
@@ -0,0 +1,33 @@
+package com.jacob.weblog.web.controller;
+
+import com.jacob.weblog.common.aspect.ApiOperationLog;
+import com.jacob.weblog.common.utils.Response;
+import com.jacob.weblog.web.service.TagService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章标签数据Controller
+ * @Date: 2024/4/21 14:54
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/tag")
+@Api(tags = "标签")
+public class TagController {
+
+    @Autowired
+    private TagService tagService;
+
+    @PostMapping("/list")
+    @ApiOperation(value = "前台获取标签列表")
+    @ApiOperationLog(description = "前台获取标签列表")
+    public Response findTagList() {
+        return tagService.findTagList();
+    }
+}

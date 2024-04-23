f11b3759d048df72a2a9f5792fe50d9af8d6d920
jdw-art <jdw17733356203@icloud.com>
2024-04-23 13:33:37 +0800

    feat(frontend-article):Thread pool configuration class

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/config/ThreadPoolConfig.java
@@ -0,0 +1,28 @@
+package com.jacob.weblog.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @Author: Jacob
+ * @Description: 线程池配置类
+ * @Date: 2024/4/23 13:17
+ * @Version: 1.0
+ */
+@Configuration
+@EnableAsync
+public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // 核心线程数
+        executor.setMaxPoolSize(20); // 最大线程数
+        executor.setQueueCapacity(100); // 队列容量
+        executor.setThreadNamePrefix("WeblogThreadPool-"); // 线程名前缀
+        executor.initialize();
+        return executor;
+    }
+}

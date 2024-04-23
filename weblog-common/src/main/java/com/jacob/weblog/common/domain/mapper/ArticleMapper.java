3d7504d2792263fe0ba72eb2b2b7fa0edaf20efb
jdw-art <jdw17733356203@icloud.com>
2024-04-23 13:30:56 +0800

    update(frontend-article):Query the previous and next articles and reading volume +1

--- weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/ArticleMapper.java
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/ArticleMapper.java
@@ -21,11 +21,11 @@ public interface ArticleMapper extends BaseMapper<ArticleDO> {
 
     /**
      * 分页查询
-     * @param current 当前页码
-     * @param size 每页展示的数据量
-     * @param title 文章标题
-     * @param startDate 开始时间
-     * @param endDate 结束时间
+     * @param current
+     * @param size
+     * @param title
+     * @param startDate
+     * @param endDate
      * @return
      */
     default Page<ArticleDO> selectPageList(Long current, Long size, String title, LocalDate startDate, LocalDate endDate) {
@@ -55,9 +55,45 @@ public interface ArticleMapper extends BaseMapper<ArticleDO> {
 
         // 构建查询条件
         LambdaQueryWrapper<ArticleDO> wrapper = Wrappers.<ArticleDO>lambdaQuery()
-                .in(ArticleDO::getId, articleIds) // 批量查询
+                .in(ArticleDO::getId, articleIds)
                 .orderByDesc(ArticleDO::getCreateTime); // 按创建时间倒叙
 
         return selectPage(page, wrapper);
+    }
+
+    /**
+     * 查询上一篇文章
+     * @param articleId
+     * @return
+     */
+    default ArticleDO selectPreArticle(Long articleId) {
+        return selectOne(Wrappers.<ArticleDO>lambdaQuery()
+                .orderByAsc(ArticleDO::getId) // 按文章 ID 倒序排列
+                .gt(ArticleDO::getId, articleId) // 查询比当前文章 ID 大的
+                .last("limit 1")); // 第一条记录即为上一篇文章
+    }
+
+    /**
+     * 查询下一篇文章
+     * @param articleId
+     * @return
+     */
+    default ArticleDO selectNextArticle(Long articleId) {
+        return selectOne(Wrappers.<ArticleDO>lambdaQuery()
+                .orderByDesc(ArticleDO::getId) // 按文章 ID 倒序排列
+                .lt(ArticleDO::getId, articleId) // 查询比当前文章 ID 小的
+                .last("limit 1")); // 第一条记录即为下一篇文章
+    }
+
+    /**
+     * 阅读量+1
+     * @param articleId
+     * @return
+     */
+    default int increaseReadNum(Long articleId) {
+        // 执行 SQL : UPDATE t_article SET read_num = read_num + 1 WHERE (id = XX)
+        return update(null, Wrappers.<ArticleDO>lambdaUpdate()
+                .setSql("read_num = read_num + 1")
+                .eq(ArticleDO::getId, articleId));
     }
 }

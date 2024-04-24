d4f42154b431822631c980052a7bb2ed51ea553e
jdw-art <jdw17733356203@icloud.com>
2024-04-24 16:17:18 +0800

    feat(front-statistic):Statistical information business service

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/controller/StatisticsController.java
@@ -0,0 +1,33 @@
+package com.jacob.weblog.web.controller;
+
+import com.jacob.weblog.common.aspect.ApiOperationLog;
+import com.jacob.weblog.common.utils.Response;
+import com.jacob.weblog.web.service.StatisticsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Jacob
+ * @Description: 统计信息业务Controller
+ * @Date: 2024/4/24 16:05
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/statistics")
+@Api(tags = "统计信息")
+public class StatisticsController {
+
+    @Autowired
+    private StatisticsService statisticsService;
+
+    @PostMapping("/info")
+    @ApiOperation(value = "前台获取统计信息")
+    @ApiOperationLog(description = "前台获取统计信息")
+    public Response findInfo() {
+        return statisticsService.findInfo();
+    }
+}

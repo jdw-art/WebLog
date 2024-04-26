4eaadfe4eecb9062ff76aa381a6331c0d7092524
jdw-art <jdw17733356203@icloud.com>
2024-04-26 22:11:00 +0800

    feat(weblog-web):Article Search VO

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/search/SearchArticlePageListReqVO.java
@@ -0,0 +1,29 @@
+package com.jacob.weblog.web.model.vo.search;
+
+import com.jacob.weblog.common.model.BasePageQuery;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @Author: Jacob
+ * @Description: 文章搜索 VO
+ * @Date: 2024/4/26 21:58
+ * @Version: 1.0
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(value = "文章搜索 VO")
+public class SearchArticlePageListReqVO extends BasePageQuery {
+    /**
+     * 查询关键词
+     */
+    @NotBlank(message = "搜索关键词不能为空")
+    private String word;
+}

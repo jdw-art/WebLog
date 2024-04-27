2f405662c6fb22562d6897138ef11939d86ef7b6
jdw-art <jdw17733356203@icloud.com>
2024-04-27 21:41:24 +0800

    update(weblog-admin):Spring event publishing and subscription ensures data consistency: implementing Lucene document addition, modification, and deletion

--- weblog-admin/src/main/java/com/jacob/weblog/admin/service/impl/AdminArticleServiceImpl.java
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/service/impl/AdminArticleServiceImpl.java
@@ -3,6 +3,9 @@ package com.jacob.weblog.admin.service.impl;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.common.collect.Lists;
 import com.jacob.weblog.admin.convert.ArticleDetailConvert;
+import com.jacob.weblog.admin.event.DeleteArticleEvent;
+import com.jacob.weblog.admin.event.PublishArticleEvent;
+import com.jacob.weblog.admin.event.UpdateArticleEvent;
 import com.jacob.weblog.admin.model.vo.article.*;
 import com.jacob.weblog.admin.service.AdminArticleService;
 import com.jacob.weblog.common.domain.dos.*;
@@ -13,6 +16,7 @@ import com.jacob.weblog.common.utils.PageResponse;
 import com.jacob.weblog.common.utils.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
@@ -47,6 +51,8 @@ public class AdminArticleServiceImpl implements AdminArticleService {
     private TagMapper tagMapper;
     @Autowired
     private ArticleTagRelMapper articleTagRelMapper;
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
 
     /**
      * 发布文章
@@ -96,6 +102,9 @@ public class AdminArticleServiceImpl implements AdminArticleService {
         // 4. 保存文章关联的标签集合
         List<String> publishTags = publishArticleReqVO.getTags();
         insertTags(articleId, publishTags);
+
+        // 发送文章发布事件
+        eventPublisher.publishEvent(new PublishArticleEvent(this, articleId));
 
         return Response.success();
     }
@@ -207,6 +216,9 @@ public class AdminArticleServiceImpl implements AdminArticleService {
 
         // 4. 删除文章-标签关联记录
         articleTagRelMapper.deleteByArticleId(articleId);
+
+        // 发布文章删除事件
+        eventPublisher.publishEvent(new DeleteArticleEvent(this, articleId));
 
         return Response.success();
     }
@@ -335,6 +347,9 @@ public class AdminArticleServiceImpl implements AdminArticleService {
         articleTagRelMapper.deleteByArticleId(articleId);
         List<String> publishTags = updateArticleReqVO.getTags();
         insertTags(articleId, publishTags);
+
+        // 发布文章修改事件
+        eventPublisher.publishEvent(new UpdateArticleEvent(this, articleId));
 
         return Response.success();
     }

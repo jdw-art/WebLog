83b9ac44303b086213b891b6a4fcc35b7e67cf97
jdw-art <jdw17733356203@icloud.com>
2024-04-30 22:21:15 +0800

    update(category):Count the total number of articles under specified categories and tags

--- weblog-admin/src/main/java/com/jacob/weblog/admin/event/subscriber/PublishArticleSubscriber.java
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/event/subscriber/PublishArticleSubscriber.java
@@ -1,6 +1,7 @@
 package com.jacob.weblog.admin.event.subscriber;
 
 import com.jacob.weblog.admin.event.PublishArticleEvent;
+import com.jacob.weblog.admin.service.AdminStatisticsService;
 import com.jacob.weblog.common.constant.Constants;
 import com.jacob.weblog.common.domain.dos.ArticleContentDO;
 import com.jacob.weblog.common.domain.dos.ArticleDO;
@@ -32,6 +33,8 @@ public class PublishArticleSubscriber implements ApplicationListener<PublishArti
     private ArticleMapper articleMapper;
     @Autowired
     private ArticleContentMapper articleContentMapper;
+    @Autowired
+    private AdminStatisticsService statisticsService;
 
     @Override
     @Async("threadPoolTaskExecutor")
@@ -63,5 +66,13 @@ public class PublishArticleSubscriber implements ApplicationListener<PublishArti
         long count = luceneHelper.addDocument(ArticleIndex.NAME, document);
 
         log.info("==> 添加文章对应 Lucene 文档结束，articleId: {}，受影响行数: {}", articleId, count);
+
+        // 重新统计各分类下文章总数
+        statisticsService.statisticsCategoryArticleTotal();
+        log.info("==> 重新统计各分类下文章总数");
+
+        // 重新统计各标签下文章总数
+        statisticsService.statisticsTagArticleTotal();
+        log.info("==> 重新统计各标签下文章总数");
     }
 }

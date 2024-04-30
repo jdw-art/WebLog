63c3fd551cf29d3a6fe8123f83c3d6c6534e37e5
jdw-art <jdw17733356203@icloud.com>
2024-04-30 22:19:35 +0800

    update(category):Add specify quantity when querying

--- weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/CategoryMapper.java
+++ weblog-common/src/main/java/com/jacob/weblog/common/domain/mapper/CategoryMapper.java
@@ -2,7 +2,10 @@ package com.jacob.weblog.common.domain.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.jacob.weblog.common.domain.dos.CategoryDO;
+
+import java.util.List;
 
 /**
  * @Author: Jacob
@@ -23,5 +26,16 @@ public interface CategoryMapper extends BaseMapper<CategoryDO> {
 
         // 执行查询
         return selectOne(wrapper);
+    }
+
+    /**
+     * 查询时指定数量
+     * @param limit
+     * @return
+     */
+    default List<CategoryDO> selectByLimit(Long limit) {
+        return selectList(Wrappers.<CategoryDO>lambdaQuery()
+                .orderByDesc(CategoryDO::getArticlesTotal) // 根据文章总数降序
+                .last(String.format("LIMIT %d", limit))); // 查询指定数量
     }
 }

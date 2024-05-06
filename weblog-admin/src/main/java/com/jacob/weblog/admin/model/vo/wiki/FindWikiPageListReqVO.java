9f9b6484eaaec6cb80b761d0774fa3bbceb4f29f
jdw-art <jdw17733356203@icloud.com>
2024-05-06 19:46:55 +0800

    feat(wiki):Query knowledge base paginated data input parameters VO

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/wiki/FindWikiPageListReqVO.java
@@ -0,0 +1,38 @@
+package com.jacob.weblog.admin.model.vo.wiki;
+
+import com.jacob.weblog.common.model.BasePageQuery;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * @Author: Jacob
+ * @Description: 查询知识库分页数据入参 VO
+ * @Date: 2024/5/6 19:39
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "查询知识库分页数据入参 VO")
+public class FindWikiPageListReqVO extends BasePageQuery {
+    /**
+     * 知识库标题
+     */
+    private String title;
+
+    /**
+     * 发布的起始日期
+     */
+    private LocalDate startDate;
+
+    /**
+     * 发布的结束日期
+     */
+    private LocalDate endDate;
+}

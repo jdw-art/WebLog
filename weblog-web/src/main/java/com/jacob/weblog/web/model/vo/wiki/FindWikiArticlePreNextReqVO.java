4939b7b94aba4affe3bf09e829111ab60661f197
jdw-art <jdw17733356203@icloud.com>
2024-05-07 20:17:13 +0800

    feat(wiki):The knowledge base obtains the previous and next articles and enters the entity class

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/wiki/FindWikiArticlePreNextReqVO.java
@@ -0,0 +1,28 @@
+package com.jacob.weblog.web.model.vo.wiki;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Jacob
+ * @Description: 知识库获取文章上下篇入参实体类
+ * @Date: 2024/5/7 19:45
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindWikiArticlePreNextReqVO {
+
+    @NotNull(message = "知识库 ID 不能为空")
+    private Long id;
+
+    @NotNull(message = "文章 ID 不能为空")
+    private Long articleId;
+
+}

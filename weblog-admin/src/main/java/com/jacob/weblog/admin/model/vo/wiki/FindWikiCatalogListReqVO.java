239dc4da8004623b38f74f5da90906ce24be111a
jdw-art <jdw17733356203@icloud.com>
2024-05-07 20:17:37 +0800

    feat(wiki):Query knowledge base directory data input VO

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/wiki/FindWikiCatalogListReqVO.java
@@ -0,0 +1,29 @@
+package com.jacob.weblog.admin.model.vo.wiki;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Jacob
+ * @Description: 查询知识库目录数据入参 VO
+ * @Date: 2024/5/7 19:18
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "查询知识库目录数据入参 VO")
+public class FindWikiCatalogListReqVO {
+
+    /**
+     * 知识库 ID
+     */
+    @NotNull(message = "知识库 ID 不能为空")
+    private Long id;
+}

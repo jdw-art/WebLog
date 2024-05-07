76ab85775619355bfe8349d8518b02c8b00cf6bf
jdw-art <jdw17733356203@icloud.com>
2024-05-07 20:17:48 +0800

    feat(wiki):Knowledge base obtains directory entry parameter entity class

---
+++ weblog-web/src/main/java/com/jacob/weblog/web/model/vo/wiki/FindWikiCatalogListReqVO.java
@@ -0,0 +1,25 @@
+package com.jacob.weblog.web.model.vo.wiki;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @Author: Jacob
+ * @Description: 知识库获取目录入参实体类
+ * @Date: 2024/5/7 19:42
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FindWikiCatalogListReqVO {
+
+    @NotNull(message = "知识库 ID 不能为空")
+    private Long id;
+
+}

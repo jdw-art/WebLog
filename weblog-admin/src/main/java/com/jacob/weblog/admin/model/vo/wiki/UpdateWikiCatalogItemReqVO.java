39a44e2a882f07f3fa736764351d3d948a04c027
jdw-art <jdw17733356203@icloud.com>
2024-05-07 20:18:40 +0800

    feat(wiki):Update knowledge base catalog data output VO

---
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/model/vo/wiki/UpdateWikiCatalogItemReqVO.java
@@ -0,0 +1,46 @@
+package com.jacob.weblog.admin.model.vo.wiki;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * @Author: Jacob
+ * @Description: 更新知识库目录数据出参 VO
+ * @Date: 2024/5/7 19:25
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateWikiCatalogItemReqVO {
+
+    /**
+     * 目录 ID
+     */
+    @NotNull(message = "目录 ID 不能为空")
+    private Long id;
+
+    private Long articleId;
+
+    @NotBlank(message = "目录标题不能为空")
+    private String title;
+
+    private Integer sort;
+
+    private Integer level;
+
+    /**
+     * 子目录
+     */
+    @Valid
+    private List<UpdateWikiCatalogItemReqVO> children;
+
+}

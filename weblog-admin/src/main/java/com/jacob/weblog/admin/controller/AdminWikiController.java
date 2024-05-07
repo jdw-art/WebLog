0da32240dfc2b587e7b45f66eb5a0a0695577e27
jdw-art <jdw17733356203@icloud.com>
2024-05-07 20:21:29 +0800

    update(wiki):Added an interface to update the top status of the knowledge base

--- weblog-admin/src/main/java/com/jacob/weblog/admin/controller/AdminWikiController.java
+++ weblog-admin/src/main/java/com/jacob/weblog/admin/controller/AdminWikiController.java
@@ -1,8 +1,6 @@
 package com.jacob.weblog.admin.controller;
 
-import com.jacob.weblog.admin.model.vo.wiki.AddWikiReqVO;
-import com.jacob.weblog.admin.model.vo.wiki.DeleteWikiReqVO;
-import com.jacob.weblog.admin.model.vo.wiki.FindWikiPageListReqVO;
+import com.jacob.weblog.admin.model.vo.wiki.*;
 import com.jacob.weblog.admin.service.AdminWikiService;
 import com.jacob.weblog.common.aspect.ApiOperationLog;
 import com.jacob.weblog.common.utils.Response;
@@ -50,5 +48,44 @@ public class AdminWikiController {
     @ApiOperationLog(description = "查询知识库分页数据")
     public Response findWikiPageList(@RequestBody @Validated FindWikiPageListReqVO findWikiPageListReqVO) {
         return wikiService.findWikiPageList(findWikiPageListReqVO);
+    }
+
+    @PostMapping("/isTop/update")
+    @ApiOperation(value = "更新知识库置顶状态")
+    @ApiOperationLog(description = "更新知识库置顶状态")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Response updateWikiIsTop(@RequestBody @Validated UpdateWikiIsTopReqVO updateWikiIsTopReqVO) {
+        return wikiService.updateWikiIsTop(updateWikiIsTopReqVO);
+    }
+
+    @PostMapping("/isPublish/update")
+    @ApiOperation(value = "更新知识库发布状态")
+    @ApiOperationLog(description = "更新知识库发布状态")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Response updateWikiIsPublish(@RequestBody @Validated UpdateWikiIsPublishReqVO updateWikiIsPublishReqVO) {
+        return wikiService.updateWikiIsPublish(updateWikiIsPublishReqVO);
+    }
+
+    @PostMapping("/update")
+    @ApiOperation(value = "更新知识库")
+    @ApiOperationLog(description = "更新知识库")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Response updateWiki(@RequestBody @Validated UpdateWikiReqVO updateWikiReqVO) {
+        return wikiService.updateWiki(updateWikiReqVO);
+    }
+
+    @PostMapping("/catalog/list")
+    @ApiOperation(value = "查询知识库目录数据")
+    @ApiOperationLog(description = "查询知识库目录数据")
+    public Response findWikiCatalogList(@RequestBody @Validated FindWikiCatalogListReqVO findWikiCatalogListReqVO) {
+        return wikiService.findWikiCatalogList(findWikiCatalogListReqVO);
+    }
+
+    @PostMapping("/catalog/update")
+    @ApiOperation(value = "更新知识库目录")
+    @ApiOperationLog(description = "更新知识库目录")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Response updateWikiCatalogs(@RequestBody @Validated UpdateWikiCatalogReqVO updateWikiCatalogsReqVO) {
+        return wikiService.updateWikiCatalogs(updateWikiCatalogsReqVO);
     }
 }

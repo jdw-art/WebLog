c05ce36ef8cbca406c52d59d90b3730336d2e30b
jdw-art <jdw17733356203@icloud.com>
2024-06-07 11:02:08 +0800

    test(weblog-jwt):modify test annotation

--- weblog-jwt/src/test/java/com/jacob/weblog/jwt/WeblogJwtApplicationTests.java
+++ weblog-jwt/src/test/java/com/jacob/weblog/jwt/WeblogJwtApplicationTests.java
@@ -3,8 +3,11 @@ package com.jacob.weblog.jwt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = WeblogJwtApplicationTests.Application.class)
 class WeblogJwtApplicationTests {
+
+    public static class Application {
+    }
 
     @Test
     void contextLoads() {

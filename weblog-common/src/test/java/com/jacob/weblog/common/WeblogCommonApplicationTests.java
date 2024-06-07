0fbff9a66cbb031d8a38a6a34832083b24debb61
jdw-art <jdw17733356203@icloud.com>
2024-06-07 11:02:02 +0800

    test(weblog-common):modify test annotation

--- weblog-common/src/test/java/com/jacob/weblog/common/WeblogCommonApplicationTests.java
+++ weblog-common/src/test/java/com/jacob/weblog/common/WeblogCommonApplicationTests.java
@@ -1,10 +1,15 @@
 package com.jacob.weblog.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = WeblogCommonApplicationTests.Application.class)
+@Slf4j
 class WeblogCommonApplicationTests {
+
+    public static class Application {
+    }
 
     @Test
     void contextLoads() {
